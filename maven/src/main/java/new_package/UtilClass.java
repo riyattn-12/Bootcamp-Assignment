486b1916cd44d2e3531e747f9707eeedf42b171d
riya <riya1@tothenew.com>
2023-03-25 16:28:14 +0530

    Maven Assignment

---
+++ maven/src/main/java/new_package/UtilClass.java
@@ -0,0 +1,13 @@
+package new_package;
+
+public class UtilClass
+{
+    public int Add(int a, int b)
+    {
+        return a+b;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a*b;
+    }
+}
